57523ed04197aae20b473ba5b47d21f730631500
Emir <celikemirhan6@gmail.com>
2024-10-06 18:26:40 +0300

    Prefix Postfix Operator Overloads

--- inc/Date.hpp
+++ inc/Date.hpp
@@ -3,6 +3,20 @@
 
 #include <iosfwd>
 #include <ctime>
+#include <array>
+#include <string_view>
+#include <iostream>
+
+constexpr std::array<std::string_view, 12> month_names = {
+    "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+    "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
+};
+constexpr std::array<std::string_view, 7> day_names = {
+    "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"
+};
+
+std::array<int, 12> days_in_month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 
 namespace project {
 	class Date {
@@ -15,8 +29,9 @@ namespace project {
 		Date(int d, int m, int y);  //6
 		explicit Date(const char* p);  //7
 		explicit Date(std::time_t timer); //8
+		
 		[[nodiscard]] int get_month_day()const; //9
-		[[nodiscard]] int get_month()const; //10
+		[[nodiscard]] std::string_view get_month()const; //10
 		[[nodiscard]] int get_year()const; //11
 		[[nodiscard]] int get_year_day()const; //12
 		[[nodiscard]] Weekday get_week_day()const; //13
@@ -25,7 +40,7 @@ namespace project {
 		Date& set_month(int month); //15
 		Date& set_year(int year); //16
 		Date& set(int day, int mon, int year); //17
-		[[nodiscard]] Date operator-(int day)const; //18
+		[[nodiscard]] Date operator-(int days)const; //18
 		Date& operator+=(int day); //19
 		Date& operator-=(int day); //20
 		Date& operator++(); //21
@@ -33,13 +48,54 @@ namespace project {
 		Date& operator--(); //23
 		Date operator--(int); //24
 
-		friend bool operator<(const Date&, const Date&); //27
-		friend bool operator==(const Date&, const Date&); //27
+		friend bool operator<(const Date& lhs, const Date& rhs){
+						if (lhs.year_ < rhs.year_) {
+				return true;
+			} else if (lhs.year_ == rhs.year_) {
+				if (lhs.month_ < rhs.month_) {
+					return true;
+				} else if (lhs.month_ == rhs.month_) {
+					return lhs.day_ < rhs.day_; // Son olarak gün karşılaştırılır
+				}
+			}
+			return false;
+		}; //27
+
+		friend bool operator==(const Date& lhs, const Date& rhs)
+		{
+			if (lhs.year_ == rhs.year_ && lhs.month_ == rhs.month_ && lhs.day_ == rhs.day_) {
+				return true;
+			} 
+			
+			return false;
+		}; //27
+
 		[[nodiscard]] static Date random_date(); //25
+
+		constexpr bool is_leap_year(int const year)const;
 		static constexpr bool isleap(int y); //26
 
-		friend std::ostream& operator<<(std::ostream& os, const Date& date); //31
+		friend std::ostream& operator<<(std::ostream& os, const Date& date)
+		{
+				    os << date.day_ << '/' << date.month_ << '/' << date.year_;
+    		return os;
+
+		} //31
 		friend std::istream& operator>>(std::istream& is, Date& date); //32
+
+		static const char* to_string(Weekday day) {
+        switch (day) {
+            case Weekday::Sunday:    return "Sunday";
+            case Weekday::Monday:    return "Monday";
+            case Weekday::Tuesday:   return "Tuesday";
+            case Weekday::Wednesday: return "Wednesday";
+            case Weekday::Thursday:  return "Thursday";
+            case Weekday::Friday:    return "Friday";
+            case Weekday::Saturday:  return "Saturday";
+        }
+        return "Unknown";
+    }
+	    
 	
 	private:
 		unsigned int day_;
@@ -55,6 +111,7 @@ namespace project {
 	[[nodiscard]] int operator-(const Date& d1, const Date& d2); //28
 	[[nodiscard]] Date operator+(const Date& date, int n); //29
 	[[nodiscard]] Date operator+(int n, const Date&); //29
+	
 	Date::Weekday& operator++(Date::Weekday& r); //30
 	Date::Weekday operator++(Date::Weekday& r, int); //30
 	Date::Weekday& operator--(Date::Weekday& r); //30
--- src/Date.cpp
+++ src/Date.cpp
@@ -4,25 +4,232 @@
 
 using namespace project;
 
+
 Date::Date(){
-    Date(1,1,Date::year_base);
+    Date(1, 1, Date::year_base);
     std::cout << "Date const." << std::endl;
 };
 
-Date::Date(int d, int m, int y):day_(d),month_(m),year_(y) {
-    std::cout << "Day: " << day_ << " Moth: " << month_ << " Year: " << year_ << std::endl;
+Date::Date(int d, int m, int y): day_(d), month_(m), year_(y) {
+    std::cout << "Day: " << day_ << " Month: " << month_ << " Year: " << year_ << std::endl;
 }
 
-Date:: Date(const char* p){
+Date::Date(const char* p){
     sscanf(p, "%d/%d/%d", &day_, &month_, &year_);
-    std::cout << "char" << "Day: " << day_ << " Moth: " << month_ << " Year: " << year_ << std::endl;
+    std::cout << "char " << "Day: " << day_ << " Month: " << month_ << " Year: " << year_ << std::endl;
     
 }
+
+int Date::get_month_day()const{
+    return day_;
+}
+
+std::string_view Date::get_month()const{
+    
+    return month_ >= 1 && month_ <= 12 ? month_names[month_ - 1] : "Invalid Month";
+}
+
+int Date::get_year()const{
+    return year_;
+}
+
+constexpr bool Date::is_leap_year(int const year)const{
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+int Date::get_year_day()const{
+
+    if (is_leap_year(year_)) {
+        days_in_month[1] = 29;  // February has 29 days in a leap year
+    }
+
+    // Calculate the day of the year
+    int day_of_year = 0;
+
+    // Add the days of the previous months
+    for (unsigned int i = 0; i < month_ - 1; ++i) {
+        day_of_year += days_in_month[i];
+    }
+
+    // Add the current day's value
+    day_of_year += day_;
+
+    return day_of_year;
+}
+
+Date::Weekday Date::get_week_day()const{
+
+    int y = year_;
+    int m = month_;
+    int d = day_;
+
+    int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+     
+    // if month is less than 3 reduce year by 1
+    if (m < 3)
+        y -= 1;
+    
+    
+    auto a = static_cast<Weekday>(((y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7));
+    return a;
+
+}
+
+Date& Date::set_month_day(int day){
+    day_ = day;
+}
+
+Date& Date::set_month(int month){
+    month_ = month;
+}
+
+Date& Date::set_year(int year){
+    year_ = year;
+}
+
+Date& Date::set(int day, int mon, int year){
+    day_ = day;
+    month_ = mon;
+    year_ = year;
+
+}
+
+[[nodiscard]] Date Date::operator-(int days)const{
+
+    int day = day_;
+    int month = month_;
+    int year = year_;
+
+    if(is_leap_year(year))
+        days_in_month[1] = 29;
+
+    while(days > 0){
+        if(day > days){
+            day -= days;
+            days = 0;
+
+        }
+        else{
+            days -= day;
+            month -=1;
+            if(month == 0){
+                year -= 1;
+                if(is_leap_year(year))
+                    days_in_month[1] = 29;
+                else
+                    days_in_month[1] = 28;
+
+            }
+
+        day = days_in_month[month -1];
+        }
+        
+    }
+
+    return Date(day, month, year);
+}
+
+Date& Date::operator+=(int day){
+
+    day_ += day;
+
+    while (day_ > (month_ == 2 && is_leap_year(year_) ? 29 : days_in_month[month_ - 1])) {
+        day_ -= (month_ == 2 && is_leap_year(year_) ? 29 : days_in_month[month_ - 1]);
+        month_++;
+
+        // If month exceeds 12, adjust the year
+        if (month_ > 12) {
+            month_ = 1;
+            year_++;
+        }
+    }
+
+    // Adjust for negative days (in case day_ becomes less than 1)
+    while (day_ < 1) {
+        month_--;
+
+        if (month_ < 1) {
+            month_ = 12;
+            year_--;
+        }
+        day_ += (month_ == 2 && is_leap_year(year_) ? 29 : days_in_month[month_ - 1]);
+    }
+
+    return *this;
+
+}
+
+Date& Date::operator-=(int day){
+
+    day_ -= day;
+
+    while (day_ < 1) {
+        month_--;
+
+        if (month_ < 1) {
+            month_ = 12;
+            year_--;
+        }
+        day_ += (month_ == 2 && is_leap_year(year_) ? 29 : days_in_month[month_ - 1]);
+    }
+
+    return *this;
+
+}
+
+Date& Date::operator++(){
+    return *this +=1;
+}
+
+Date Date::operator++(int){
+    Date temp = *this;
+    ++(*this);
+    return temp;
+
+}
+
+Date& Date::operator--(){
+    return *this -=1;
+}
+
+Date Date::operator--(int){
+    Date temp = *this;
+    --(*this);
+    return temp;
+}
+
 
 
 int main(){
     // Date mydate;
+    Date mydateint_(02,10,2024);
+    Date mydate_("10/03/2025");
 
-    Date mydate_("10/03/1999");
+    std::cout << mydate_.get_year() << "\n";
+    std::cout << mydate_.get_month() << "\n";
+    std::cout << mydate_.get_year_day() << "\n";
+    std::cout << Date::to_string(mydate_.get_week_day()) << "\n";
 
+    Date new_date = mydate_ - 15;  // Subtract 15 days
+    std::cout << new_date.get_year_day() << new_date.get_month() << new_date.get_year() << "\n";          // Print the new date
+
+    Date date1(15, 10, 2020);
+    Date date2(16, 10, 2020);
+
+    if (date1 < date2) {
+        std::cout << date1 << " is earlier than " << date2 << std::endl;
+    }
+    else {
+        std::cout << date1 << " is not earlier than " << date2 << std::endl;
+    }
+        if (date1 == date2) {
+        std::cout << date1 << " is same date " << date2 << std::endl;
+    }
+    else {
+        std::cout << date1 << " is not same date " << date2 << std::endl;
+    }
+
+    Date today(28, 2, 2020); // February 28, 2020 (leap year)
+    today += 5;              // Adding 5 days (should move to March 4, 2020)
+    std::cout << "New date is: " << today << std::endl;
 }
